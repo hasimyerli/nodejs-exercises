63b089fb86a8f3959a800e35d9bef77013023e89
Haşim Yerli <hasimyerli@JW9RC9F3VP.home>
2022-04-15 16:26:23 +0300

    add env config

--- jwt-example/Service/JwtService.js
+++ jwt-example/Service/JwtService.js
@@ -1,23 +1,20 @@
 const jwt = require("jsonwebtoken");
 
-const jwtSecretKey = "my_secret_key"
-const jwtAlgorithm = "HS256"
-const jwtExpirySeconds = 300 // expire after 300 seconds
-
 const jwtService = {
     generateToken: function(data) {
         try {
-            return jwt.sign(data, jwtSecretKey, {
-                algorithm: jwtAlgorithm,
-                expiresIn: jwtExpirySeconds,
+            return jwt.sign(data, process.env.JWT_SECRET_KEY, {
+                algorithm: process.env.JWT_ALGORITHM,
+                expiresIn: process.env.JWT_EXPIRY_SECONDS,
             })
         } catch(e) {
+            console.log(e)
             return null
         }
     },
     verifyToken: function(token) {
         try {
-            return jwt.verify(token, jwtSecretKey)
+            return jwt.verify(token, process.env.JWT_SECRET_KEY)
         } catch(e) {
             console.log(e)
             return {}
--- jwt-example/server.js
+++ jwt-example/server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const jwtService = require("./Service/JwtService")
+const dotenv = require('dotenv');
+dotenv.config();
 
 const token = jwtService.generateToken({info: "jwt example"})
 console.info("token : " + token)
